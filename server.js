bd601cfeefcc97ffc78027f62ba2f0485728a435
Neo Temores <neo@Mac.lan1>
2022-07-07 12:04:06 -0700

    created basic server, connection & controller

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const controller = require('./src/backend/controller');
+
+const path = require('path');
+
+const PORT = process.env.PORT || 8000;
+
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('build'))
+}
+
+app.listen(PORT, (err) => {
+    if (err) return console.log(err);
+    console.log(`Listening on port: ${PORT}`)
+})
+
+app.get('/test', controller.testRoute);
+
+app.get('/api/users', controller.getAllUsers);
+
+app.get('/api/users/:id', controller.getOneUserById)
+
+
+
+
+//! uncomment this route before deploying.
+// app.get('*', (_, res) => {
+//     res.sendFile(path.join(__dirname, '/build/index.html'))
+// });
+
+app.use((_, res) => {
+    res.status(404);
+    res.setHeader('Content-type', 'text/plain');
+    res.send('Not Found')
+})
--- src/backend/connection.js
+++ src/backend/connection.js
@@ -2,10 +2,9 @@ const { Pool } = require('pg')
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-
 })
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     pool.ssl = { rejectUnauthorized: false }
 }
 
