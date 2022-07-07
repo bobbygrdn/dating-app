e424d0b19e1fa224cb0525d0a995f839981d09c7
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-07 10:58:46 -0700

    Creates users table using SQL command

---
+++ src/backend/migration.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    age INT,
+    height INT,
+    body_type TEXT,
+    favorite_program_language TEXT,
+    gender TEXT,
+    bio TEXT,
+    sexuality TEXT,
+    location TEXT
+)
