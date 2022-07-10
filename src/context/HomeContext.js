bf4dde8a44036a1fa6cbbf885801770f742be1e1
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-10 14:50:41 -0700

    Adds single user state

--- src/context/HomeContext.js
+++ src/context/HomeContext.js
@@ -6,15 +6,21 @@ const HomeContext = createContext()
 export const HomeProvider = ({children}) => {
     // state
     const [users, setUsers] = useState(null)
+    const [singleUser, setSingleUser] = useState(null);
 
     // functionality / functions
     const addUsers = (data) => {
         setUsers(data);
     }
+    const addSingleUser = (data) => {
+        setSingleUser(data);
+    }
         
     return <HomeContext.Provider value={{
         users,
-        addUsers
+        addUsers,
+        singleUser,
+        addSingleUser
     }}>
         {children}
     </HomeContext.Provider>
