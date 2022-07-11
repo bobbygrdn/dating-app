4f5b6205137b0e39a97c05490f7e79e00c113553
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-11 08:12:31 -0700

    Adds fetch calls to set the initial state for single user and users

--- src/Components/home/Home.jsx
+++ src/Components/home/Home.jsx
@@ -3,11 +3,11 @@ import Users from './UsersContainer';
 import '../../ComponentStyles/Home.css'
 import {useContext} from 'react'
 import HomeContext from '../../context/HomeContext';
-
+import SingleUserModal from './SingleUserModal';
 
 function Home() {
 
-    const {addUsers} = useContext(HomeContext)
+    const {addUsers, singleModal, addSingleUser} = useContext(HomeContext)
 
     useEffect(() => {
         fetch('https://find-luv.herokuapp.com/api/users')
@@ -15,11 +15,19 @@ function Home() {
         .then(data => addUsers(data))
     }, []);
 
+    useEffect(() => {
+        fetch('https://find-luv.herokuapp.com/api/users')
+        .then(response => response.json())
+        .then(data => addSingleUser(data[0]))
+    }, [])
+
     return (
         <div>
         <Users />
+        <SingleUserModal show={singleModal} />
         </div>
     )
+
 }
 
 export default Home
