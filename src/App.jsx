53df523238f2cb04f1b2402e3b881119b6973c3c
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-11 08:28:45 -0700

    Renames home component to discover component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
 import Navbar from './Components/navbar/Navbar.jsx'
-import Home from './Components/home/Home.jsx'
 import SearchResults from './Components/search-results/SearchResults.jsx'
 import Matches from './Components/matches/Matches.jsx'
 import Inbox from './Components/inbox/Inbox.jsx'
 import PendingConnections from './Components/pending-connections/PendingConnections.jsx'
 import MyProfile from './Components/profile/MyProfile.jsx'
 import {HomeProvider} from './context/HomeContext'
+import Discover from './Components/home/Discover.jsx';
 
 function App() {
 
@@ -18,7 +18,7 @@ function App() {
 
             <div className='App-container'>
                 <Routes>
-                    <Route path='/' element={<Home />} />
+                    <Route path='/' element={<Discover />} />
                     <Route path='/search-results' element={<SearchResults />} />
                     <Route path='/matches' element={<Matches />} />
                     <Route path='/inbox' element={<Inbox />} />
