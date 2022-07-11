7692695d91d7fb60fe9f433508603bc74844054a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-11 16:03:22 -0700

    Creates basic nav bar component for discover page

---
+++ src/Components/home/DiscoverNavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import DiscoverContext from "../../context/DiscoverContext";
+
+function DiscoverNavBar () {
+
+    return (
+        <nav>1</nav>
+    )
+}
+
+export default DiscoverNavBar
