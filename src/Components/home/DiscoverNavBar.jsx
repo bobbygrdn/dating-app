700901b9d96ac5db7c3da9ce1ae1463a83465d4a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-12 13:44:08 -0700

    Changes the component to properly render a nav bar with page numbers

--- src/Components/home/DiscoverNavBar.jsx
+++ src/Components/home/DiscoverNavBar.jsx
@@ -1,10 +1,23 @@
-import React from "react";
-// import DiscoverContext from "../../context/DiscoverContext";
+import React, { useContext } from "react";
+import DiscoverNavBarNumbers from "./DiscoverNavBarNumbers";
+import DiscoverContext from "../../context/DiscoverContext";
 
 function DiscoverNavBar () {
+    const {pages, setPage} = useContext(DiscoverContext)
+
+    const handleClick = (e) => {
+        // setPage(e.target.id)
+        console.log(e.target.id);
+    }
 
     return (
-        <nav>1</nav>
+            <div className="discoverNav">
+                {pages.map((elem) => {
+                    return (
+                        <DiscoverNavBarNumbers elem={elem} key={elem} handleClick={handleClick} />
+                    )
+                 })}
+            </div>
     )
 }
 
