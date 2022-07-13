9f01e4e8672d6844bf018ac2ccb82c0f0b993ece
Neo Temores <neo@Mac.lan1>
2022-07-13 08:15:27 -0700

    working on displaying thread cards

--- src/Components/inbox/Inbox.jsx
+++ src/Components/inbox/Inbox.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import './inboxStyles.css'
+import MsgLoadingThreads from './MsgLoadingThreads.jsx'
+import MsgNoThreads from './MsgNoThreads.jsx'
 
 function Inbox({ dummyUser, setDummyUser }) {
 
     const [threads, setThreads] = useState([])
+
     useEffect(() => {
         fetchAllUserThreads()
-    }, [threads])
+    }, [])
 
     const fetchAllUserThreads = () => {
         fetch(`https://find-luv.herokuapp.com/api/threads/user/${dummyUser['user_id']}`)
@@ -14,13 +17,46 @@ function Inbox({ dummyUser, setDummyUser }) {
             .then(data => setThreads(data))
     }
 
+    const obtainUserIds = () => {
+        let arrayOfUserIdsToFetch = [];
+
+        threads.forEach(item => {
+            item.recipient_user_id === dummyUser.user_id ? arrayOfUserIdsToFetch = [item.sender_user_id, ...arrayOfUserIdsToFetch] : arrayOfUserIdsToFetch = [item.recipient_user_id, ...arrayOfUserIdsToFetch]
+        })
+
+        fetchUserProfileData(arrayOfUserIdsToFetch)
+    }
+
+    const fetchUserProfileData = (arrayOfIds) => {
+        let arrayOfProfileData = []
+
+        arrayOfIds.forEach(item => {
+            fetch(`https://find-luv.herokuapp.com/api/users/${item}`)
+                .then(res => res.json())
+                .then(data => arrayOfProfileData.push(data))
+        })
+
+        displayThreadCards(arrayOfProfileData)
+    }
+
+    const displayThreadCards = (arrayOfProfileData) => {
+        arrayOfProfileData.forEach(profile => {
+            console.log(profile)
+            return (
+                <p>{profile.user_id}</p>
+            )
+        })
+    }
     return (
         <div className='inbox-main-container'>
-            {threads.map(thread => {
+            {threads.length > 0 ? obtainUserIds() : <MsgNoThreads />}
+
+            {/* {threads.map(thread => {
                 return (
                     <p>{thread['recipient_user_id']}</p>
                 )
-            })}
+            })} */}
+
         </div>
     )
 }
---
+++ src/Components/inbox/MsgNoThreads.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MsgNoThreads() {
+    return (
+        <div>You don't have any Threads! Don't be shy, get out there and connect!</div>
+    )
+}
+
+export default MsgNoThreads
