c88c9d21439bcd11474624b56d00931cef385e5f
Neo Temores <neo@Neos-Mac.local>
2022-07-13 14:46:46 -0700

    thread render test

--- src/Components/inbox/DisplayThreads.jsx
+++ src/Components/inbox/DisplayThreads.jsx
@@ -3,22 +3,22 @@ import React, { useEffect } from 'react'
 function DisplayThreads({ arrayOfProfiles }) {
     console.log(arrayOfProfiles)
 
-    useEffect(() => {
-        test()
-    }, [arrayOfProfiles])
+    // useEffect(() => {
+    //     test()
+    // }, [arrayOfProfiles])
 
-    function test() {
-        return (
-            <>
-                {arrayOfProfiles.map((elem) => (
-                    <div className='threadCard'>
-                        <img src={elem.profile_pic_url} alt="thread-card-pic" />
-                    </div>
-                ))}
 
-            </>
-        );
-    }
+    return (
+        <>
+            {arrayOfProfiles.map((elem) => (
+                <div className='threadCard'>
+                    <img src={elem.profile_pic_url} alt="thread-card-pic" />
+                </div>
+            ))}
+
+        </>
+    );
+
 
 
 }
