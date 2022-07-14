c321ab701fb5434eece3cb645c2c983f97a5ac28
AnthonyWright17 <awright032317@gmail.com>
2022-07-14 13:37:05 -0600

    More Regex fixes

--- src/Components/Landing/CreateUserForm.jsx
+++ src/Components/Landing/CreateUserForm.jsx
@@ -81,7 +81,7 @@ const CreateUserForm = () => {
       placeholder: "Password",
       errorMessage: "Password must be 8-20 characters and should include atleast 1 letter, 1 number and 1 special character!",
       label: "Password",
-      pattern: `/^(?=.*?[A-Z])(?=.?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])[A-Za-z0-9#?!@$%^&*-].{8,20}$/g`,
+      pattern: `^(?=.*?[A-Z])(?=.?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])[A-Za-z0-9#?!@$%^&*-].{8,20}$`,
       required: true
     },
 
