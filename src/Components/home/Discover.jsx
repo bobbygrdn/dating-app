a502e064f1f5794568a16dcd950fb66c31527220
Neo Temores <neo@Mac.lan1>
2022-07-14 07:53:42 -0700

    deployed

--- src/Components/home/Discover.jsx
+++ src/Components/home/Discover.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import Users from './UsersContainer';
 import '../../ComponentStyles/Discover.css'
-import {useContext} from 'react'
+import { useContext } from 'react'
 import DiscoverContext from '../../context/DiscoverContext';
 import SingleUserModal from './SingleUserModal';
 import SearchModal from './SearchModal';
@@ -10,31 +10,30 @@ import LandingContext from '../../context/LandingContext'
 function Discover() {
 
     /* Destructuring the context object. */
-    const {addUsers, singleModal, addSingleUser, searchModal} = useContext(DiscoverContext)
+    const { addUsers, singleModal, addSingleUser, searchModal } = useContext(DiscoverContext)
 
-    const {userData} = useContext(LandingContext)
+    const { userData } = useContext(LandingContext)
 
-   /* Fetching the data from the API and adding it to the state. */
+    /* Fetching the data from the API and adding it to the state. */
     useEffect(() => {
         fetch('https://find-luv.herokuapp.com/api/users')
-        .then(response => response.json())
-        .then(data => addUsers(data))
+            .then(response => response.json())
+            .then(data => addUsers(data))
     }, []);
 
     /* Fetching the data from the API and adding it to the state. */
     useEffect(() => {
         fetch('https://find-luv.herokuapp.com/api/users')
-        .then(response => response.json())
-        .then(data => addSingleUser(data[0]))
+            .then(response => response.json())
+            .then(data => addSingleUser(data[0]))
     }, [])
 
     /* Returning the Users component, the SingleUserModal component, and the SearchModal component. */
-    console.log(userData);
     return (
         <div>
-        <Users />
-        <SingleUserModal show={singleModal} />
-        <SearchModal show={searchModal} />
+            <Users />
+            <SingleUserModal show={singleModal} />
+            <SearchModal show={searchModal} />
         </div>
     )
 
