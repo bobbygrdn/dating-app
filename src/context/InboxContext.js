18e8a63f0195b037c7d50c977fdf4c42e7cb2fb7
Neo Temores <neo@Neos-Mac.local>
2022-07-14 11:55:24 -0700

    context wip

---
+++ src/context/InboxContext.js
@@ -0,0 +1,28 @@
+import { useState, createContext, useEffect, useContext } from 'react';
+import LandingContext from './LandingContext';
+
+const InboxContext = createContext()
+
+export const InboxProvider = ({ children }) => {
+
+    const [threads, setThreads] = useState(null)
+
+    const { userData } = useContext(LandingContext)
+
+    const fetchAllUserThreads = () => {
+        fetch(`https://find-luv.herokuapp.com/api/threads/user/${userData['user_id']}`)
+            .then(res => res.json())
+            .then(data => setThreads(data))
+            .catch(err => console.log(err))
+    }
+
+    return <InboxContext.Provider value={{
+        threads,
+        setThreads,
+        fetchAllUserThreads
+    }}>
+        {children}
+    </InboxContext.Provider>
+}
+
+export default InboxContext
