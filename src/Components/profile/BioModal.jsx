fa980b9476708c0f0fe81728f8279ce4b674fa4e
Neo Temores <neo@Neos-Mac.local>
2022-07-14 19:41:04 -0700

    modals displaying, need css

---
+++ src/Components/profile/BioModal.jsx
@@ -0,0 +1,58 @@
+import React, { useRef, useState } from 'react';
+import ReactDom from "react-dom";
+
+const BioModal = ({ setShowBioModal, userData }) => {
+
+    const [formData, setFormData] = useState({
+        textContent: userData.bio
+    })
+
+    const modalRef = useRef();
+    const closeModal = (e) => {
+        if (e.target === modalRef.current) {
+            return setShowBioModal(false)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const handleChange = (e) => {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+    return ReactDom.createPortal(
+
+        <div className='modalContainer' ref={modalRef} onClick={closeModal}>
+
+            <div className='editDataContainer'>
+
+                <form onSubmit={handleSubmit} >
+                    <input type='submit' value='Update Bio'
+                        id='editDataSubmitBtn' />
+                    <textarea
+                        id='editDataTextArea'
+                        className="textareaNewPost"
+                        name="textContent"
+                        value={formData.textContent}
+                        onChange={handleChange}
+                        rows="7"
+                        cols="40"
+                    /> <br />
+
+
+                </form>
+                <button className='modalCloseBtn' onClick={() => setShowBioModal(false)}> X </button>
+            </div>
+        </div>,
+        document.getElementById('portal')
+    )
+
+}
+
+export default BioModal
