c04abdd248bf193fef7a96d84735e5494325b52b
Neo Temores <neo@Mac.lan1>
2022-07-16 21:07:32 -0700

    added ternary before rendering Connection component

--- src/Components/pending-connections/PendingConnections.jsx
+++ src/Components/pending-connections/PendingConnections.jsx
@@ -10,18 +10,13 @@ function PendingConnections() {
     return (
         <div className='pending-connections-container'>
             <div className='connections-container'>
-                {/* {userData.liked.map((elem) => {
-                return (
-                    <Connection elem={elem} key={elem.user_id} />
-                    )
-                })} */}
-
+             
                 {userData.liked ?  userData.liked.map((elem) => {
                 return (
                     <Connection elem={elem} key={elem.user_id} />
                     )
                 }) : null}
-                
+
             </div>
         </div>
     )
