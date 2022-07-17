720396095ae2659bcb291bde1cd6e513c66bdd51
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-16 19:25:27 -0700

    Updates the way the user data liked value is mapped out

--- src/Components/pending-connections/PendingConnections.jsx
+++ src/Components/pending-connections/PendingConnections.jsx
@@ -6,12 +6,11 @@ import Connection from './Connection'
 function PendingConnections() {
 
     const { userData } = useContext(LandingContext)
-    let connectionsMap = [userData.liked]
 
     return (
         <div className='pending-connections-container'>
             <div className='connections-container'>
-                {connectionsMap.map((elem) => {
+                {userData.liked.map((elem) => {
                 return (
                     <Connection elem={elem} key={elem.user_id} />
                     )
