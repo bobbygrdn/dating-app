311a28cb6e5488e9eb82af4afc381a8bfcbcc14f
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-18 15:59:52 -0700

    Changes the state look up of the use effect hook

--- src/Components/pending-connections/PendingConnections.jsx
+++ src/Components/pending-connections/PendingConnections.jsx
@@ -14,7 +14,7 @@ function PendingConnections() {
 
     /* Fetching the data from the API and setting the state of pending to the data. */
     useEffect(()=> {
-            if(userData.liked !== null) {
+            if(pending === null) {
             fetch(`https://find-luv.herokuapp.com/api/pending/${userData.liked}`)
             .then(response => response.json())
             .then(data => setPending(data))
