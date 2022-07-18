364309b7fd7106effcb40f92ae0f6415b0e2abe6
Neo Temores <neo@Neos-Mac.local>
2022-07-17 22:58:23 -0700

    msg system displaying

--- src/Components/inbox/ThreadMsgModal.jsx
+++ src/Components/inbox/ThreadMsgModal.jsx
@@ -1,11 +1,18 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import ReactDom from "react-dom";
 
-function ThreadMsgModal({ setShowThreadMsgModal, displayedMessages }) {
+function ThreadMsgModal({ setShowThreadMsgModal, displayedMessages, threadMsgUserInfo, userData }) {
+
+    const [replyMsg, setReplyMsg] = useState({
+        msgText: ''
+    })
 
     useEffect(() => {
         console.log(displayedMessages)
-    }, [displayedMessages])
+
+        var elem = document.getElementById('msgDisplayBox');
+        elem.scrollTop = elem.scrollHeight;
+    }, [threadMsgUserInfo, setShowThreadMsgModal, displayedMessages])
 
     const modalRef = useRef();
     const closeModal = (e) => {
@@ -15,39 +22,55 @@ function ThreadMsgModal({ setShowThreadMsgModal, displayedMessages }) {
     }
     const handleSubmit = (e) => {
         e.preventDefault()
-        // changeUserData(formData)
         // fetch(`https://find-luv.herokuapp.com/api/userdata/bio/${userData.user_id}`, {
         //     method: "PATCH",
         //     headers: { 'Content-Type': 'application/json' },
         //     body: JSON.stringify(formData)
         //   })
         //   .catch(err => console.log(err))
-        setShowThreadMsgModal(false)
     }
 
     const handleChange = (e) => {
-        // setFormData(prevFormData => {
-        //     return {
-        //         ...prevFormData,
-        //         [e.target.name]: e.target.value
-        //     }
-        // })
+        setReplyMsg(prevFormData => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value
+            }
+        })
     }
     return ReactDom.createPortal(
 
         <div className='modalContainer' ref={modalRef} onClick={closeModal}>
 
-            <div className='editDataContainer'>
+            <div className='msgThreadContainer'>
+                <div className='threadModalHeader'>
+                    <img className="msgMiniPic" src={threadMsgUserInfo.pic} alt='profile-pic' />
+                    <p>{threadMsgUserInfo.name}</p>
+                </div>
+                <div id="msgDisplayBox" className='msgDisplayBox'>
+                    {displayedMessages.map((elem, index) => {
+                        console.log(elem)
+                        return (
+                            <div className={elem.sent_from_user_id === userData.user_id ? 'right' : 'left'}>
+                                <div className='individualMsgDiv'>
+                                    <p className='msgContent'>{elem.content}</p>
+                                    <p className='msgDateTimeStamp'>{elem.date_stamp} {elem.time_stamp}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
 
-                <form onSubmit={handleSubmit} className='updateUserDataForm' >
+                </div>
+
+                <form onSubmit={handleSubmit} className='replyMsgForm' >
 
                     <textarea
-                        id='editDataTextArea'
+                        id='replyMsgTextArea'
                         className="textareaNewPost"
-                        name="bio"
-                        // value={formData.bio}
+                        name="msgText"
+                        value={replyMsg.msgText}
                         onChange={handleChange}
-                        rows="7"
+                        rows="3"
                         cols="40"
                     />
                     <input type='submit' value='Reply'
