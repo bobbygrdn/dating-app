e524e274dc35291f3d39f2d1ca04f1191afdcf25
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-07-17 20:06:40 -0700

    Brings the discover provider up one level to the index file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { React, useContext, useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/navbar/Navbar.jsx";
-import Matches from "./Components/matches/Matches.jsx";
 import Inbox from "./Components/inbox/Inbox.jsx";
 import PendingConnections from "./Components/pending-connections/PendingConnections.jsx";
 import MyProfile from "./Components/profile/MyProfile.jsx";
@@ -9,7 +8,6 @@ import Landing from "./Components/Landing/Landing.jsx";
 import Discover from "./Components/home/Discover.jsx";
 import CreateUserForm from "./Components/Landing/CreateUserForm";
 import Login from "./Components/Landing/Login";
-import { DiscoverProvider } from "./context/DiscoverContext.js";
 import LandingContext from "./context/LandingContext";
 import InboxContext from "./context/InboxContext.js";
 import "../src/ComponentStyles/CreateSlashLogin.css";
@@ -41,7 +39,7 @@ function App() {
         return (
 
             <div className='App-container'>
-                <DiscoverProvider>
+
                     <Navbar userData={userData} />
 
                     <Routes>
@@ -52,7 +50,7 @@ function App() {
                         <Route path='/profile' element={<MyProfile />} />
 
                     </Routes>
-                </DiscoverProvider>
+
             </div>
 
         )
