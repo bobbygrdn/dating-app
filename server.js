83c58e47daae2004a6708ff5d3ffa15346b9315f
Neo Temores <neo@Mac.lan1>
2022-07-18 21:44:20 -0700

    deploy

--- server.js
+++ server.js
@@ -700,8 +700,8 @@ app.post("/api/threads", async (req, res) => {
   const { recipient_user_id, sender_user_id } = req.body
   try {
     let client = await pool.connect();
-    let data = await client.query(`INSERT INTO threads(recipient_user_id, sender_user_id) VALUES ('${recipient_user_id}','${sender_user_id}')`)
-    res.send(req.body);
+    let data = await client.query(`INSERT INTO threads(recipient_user_id, sender_user_id) VALUES ('${recipient_user_id}','${sender_user_id}') OUTPUT inserted.* SELECT 1`)
+    res.send(data);
     client.release();
   } catch (error) {
     console.error(error)
