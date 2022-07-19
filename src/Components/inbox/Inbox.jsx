5374378325f706004b1a94933cb1f3c135cdc393
Neo Temores <neo@Mac.lan1>
2022-07-19 08:35:51 -0700

    deploy

--- src/Components/inbox/Inbox.jsx
+++ src/Components/inbox/Inbox.jsx
@@ -5,14 +5,17 @@ import MsgNoThreads from './MsgNoThreads.jsx'
 import LandingContext from "../../context/LandingContext";
 import InboxContext from '../../context/InboxContext'
 
+
 function Inbox() {
+    const { fetchAllUserThreads } = useContext(InboxContext)
+
     const { userData } = useContext(LandingContext)
 
     const { threads, orderedProfiles } = useContext(InboxContext);
 
     return (
         <div className='inbox-main-container'>
-            {threads.length > 0 ? <DisplayThreads threads={threads} orderedProfiles={orderedProfiles} userData={userData} /> : <MsgNoThreads />}
+            {threads.length > 0 && orderedProfiles.length > 0 ? <DisplayThreads threads={threads} orderedProfiles={orderedProfiles} userData={userData} /> : <MsgNoThreads />}
         </div>
     )
 
